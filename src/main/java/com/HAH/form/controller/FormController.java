a2ed4586a3ef60d718201eedb8b86dc5d97b08bf
hhtun137 <htunaunghlaing199@gmail.com>
2024-04-16 19:57:13 +0630

    starting spring form in project

---
+++ src/main/java/com/HAH/form/controller/FormController.java
@@ -0,0 +1,16 @@
+package com.HAH.form.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("form")
+public class FormController {
+
+	@GetMapping
+	void index() {
+		
+	}
+	
+}
---
+++ src/main/java/com/HAH/form/model/UserInput.java
@@ -0,0 +1,14 @@
+package com.HAH.form.model;
+
+import lombok.Data;
+
+@Data 
+public class UserInput {
+
+	private int id;
+	private String name;
+	private String phone;
+	private String email;
+	private String password;
+	private String course;
+}
