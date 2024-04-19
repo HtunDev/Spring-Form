cbb2a85ab8044b2bc04ecd3ad02781bc8f434530
hhtun137 <htunaunghlaing199@gmail.com>
2024-04-19 23:37:04 +0630

    using bean validation api(hibernate validator)

--- src/main/java/com/HAH/form/root/dto/UserInput.java
+++ src/main/java/com/HAH/form/root/dto/UserInput.java
@@ -2,6 +2,12 @@ package com.HAH.form.root.dto;
 
 import java.time.LocalDate;
 
+import javax.validation.constraints.AssertTrue;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
 import org.springframework.format.annotation.DateTimeFormat;
 
 import lombok.Data;
@@ -10,15 +16,20 @@ import lombok.Data;
 public class UserInput {
 
 	private int id;
+	@NotBlank(message = "Please Enter Name")
 	private String name;
+	@Pattern(regexp = "09(-\\d{4}){2}", message = "Invalid Phone Number")
 	private String phone;
+	@Email(message = "Invalid Email Address")
 	private String email;
 	private String password;
+	@NotNull(message = "Pls select course")
 	private Course course;
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private LocalDate registrationDate;
 	private Gender gender;
 	private String[] foundation;
+	@AssertTrue(message = "You have need to agree the rules.")
 	private boolean agree;
 	private String remark;
 
